6678ab14b341ee4637bcf2b0f658e7eafaba5004
Hassan Ali <hassansiddiqui03024053707@gmail.com>
2024-08-17 17:01:53 +0500

    front+back integration 1

--- Front/src/components/Contact.jsx
+++ Front/src/components/Contact.jsx
@@ -1,15 +1,37 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
 
-  const [name,setName]=useState("");
-  const [email,setEmail]=useState("");
-  const [phone,setPhone]=useState("");
-  const [message,setMessage]=useState("");
-
-  const handleSendMessage=(e)=>{
+  const handleSendMessage = async (e) => {
     e.preventDefault();
-  }
+
+    await axios
+      .post(
+        "http://localhost:4000/api/v1/message/send",
+        { name, email, phone, message },
+        {
+          withCredentials: true,
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+      .then((res) => {
+        setName("");
+        setEmail("");
+        setPhone("");
+        setMessage("");
+        toast.success(res.data.message);
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error(error.response.data.message);
+      });
+  };
 
   return (
     <section className="contact">
@@ -18,14 +40,34 @@ const Contact = () => {
           <form onSubmit={handleSendMessage}>
             <h2>CONTACT US</h2>
             <div>
-              <input type="text" value={name} placeholder="Your name" onChange={(e)=>setName(e.target.value)}/>
-              <input type="email" value={email} placeholder="Your Email" onChange={(e)=>setEmail(e.target.value)}/>
-              <input type="number" value={phone} placeholder="Phone" onChange={(e)=>setPhone(e.target.value)}/>
+              <input
+                type="text"
+                value={name}
+                placeholder="Your name"
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type="email"
+                value={email}
+                placeholder="Your Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="number"
+                value={phone}
+                placeholder="Phone"
+                onChange={(e) => setPhone(e.target.value)}
+              />
             </div>
-            <textarea rows="10" value={message} placeholder="Your Message" onChange={(e)=>setMessage(e.target.value)}/>
-              <button className="btn" type="submit">
-                Send
-              </button>
+            <textarea
+              rows="10"
+              value={message}
+              placeholder="Your Message"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <button className="btn" type="submit">
+              Send
+            </button>
           </form>
         </div>
       </div>
--- Front/src/components/Donate.jsx
+++ Front/src/components/Donate.jsx
@@ -1,19 +1,46 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 // import {Link} from "react-router-dom"
 
 const Donate = () => {
-
   const [amount, setAmount] = useState();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const [disableBtn, setDisableBtn] = useState(false)
+  const [disableBtn, setDisableBtn] = useState(false);
+
+  const handleCheckout = async (e) => {
+    e.preventDefault();
+
+    try {
+      setDisableBtn(true);
+
+      await axios
+        .post(
+          "http://localhost:4000/api/v1/checkout",
+          { name, email, message, amount },
+          {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data);
+          window.location.href = res.data.result.url;
+        });
+    } catch (error) {
+      setDisableBtn(false);
+      console.error(error);
+    }
+  };
 
   return (
     <>
       <section className="donate">
-        <form>
+        <form onSubmit={handleCheckout}>
           <div>
             <img src="/logo.png" alt="logo" />
           </div>
@@ -44,7 +71,9 @@ const Donate = () => {
             onChange={(e) => setMessage(e.target.value)}
             placeholder="Enter any message"
           />
-          <button disabled={disableBtn} type="submit" className="btn">Donate {amount ? `$${amount}` : "$0"}</button>
+          <button disabled={disableBtn} type="submit" className="btn">
+            Donate {amount ? `$${amount}` : "$0"}
+          </button>
         </form>
       </section>
     </>
