c44d40cf6464d35115e009eda4dcc7593e82bd38
Hassan Ali <hassansiddiqui03024053707@gmail.com>
2024-08-18 23:29:39 +0500

    homepage frontend final

--- Front/src/miniComponents/Hero.jsx
+++ Front/src/miniComponents/Hero.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import {useEffect} from "react"
 import { Link } from "react-router-dom";
 import "./Hero.css";
+import lottie from "lottie-web"
 
 const Hero = () => {
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: document.getElementById("lottie-animation"), // the container where the animation will be rendered
+      renderer: "svg", // renders the animation as SVG
+      loop: true, // set to true for looping the animation
+      autoplay: true, // set to true for auto-playing the animation
+      path: "/flag.json" // the path to the Lottie animation JSON file
+    });
+  }, []);
+
   return (
     <section className="hero">
       <div className="banner">
@@ -18,8 +31,9 @@ const Hero = () => {
           Donate Now
         </Link>
       </div>
-      <div className="banner">
-        <img src="/hero.png" alt="hero" />
+      <div className="banner" id="banner1">
+        {/* <img src="/hero.png" alt="hero" /> */}
+        <div id="lottie-animation" className="flag"></div>
       </div>
     </section>
   );
