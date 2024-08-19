afad6f3cd2ddebe6a0bdb8baa9ca6bb2402568c0
Hassan Ali <hassansiddiqui03024053707@gmail.com>
2024-08-19 15:09:32 +0500

    about page final1

--- Front/src/components/About.jsx
+++ Front/src/components/About.jsx
@@ -1,24 +1,30 @@
 import React from "react";
+import "./About.css"
 
 const About = () => {
   return (
     <section className="about">
       <div className="hero">
         <div className="banner">
-          <h1>Our Story</h1>
+          <h1><span id="our">Our</span> Story</h1>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consequuntur magni explicabo totam pariatur molestias unde ducimus
-            asperiores ipsum modi nesciunt illum voluptatem nam tenetur facere,
-            debitis rem accusamus tempora culpa. Ratione soluta, facilis non
-            sequi perferendis odio itaque voluptatem libero nulla nostrum
-            assumenda sit accusantium placeat repellendus officia similique esse
-            qui ut suscipit consectetur laborum. Modi laborum officia sapiente
-            ad.
+            Our organization is dedicated to providing crucial support and aid
+            to the Palestinian people. Founded on the principles of compassion,
+            justice, and human rights, we work tirelessly to improve the lives
+            of those affected by ongoing humanitarian challenges in Palestine.
+            Our efforts focus on delivering essential resources, medical aid,
+            educational support, and community development projects. We believe
+            in empowering local communities and fostering sustainable solutions
+            that respect Palestinian culture and traditions. Through
+            transparency, accountability, and collaborative partnerships, we
+            strive to make a lasting positive impact and build a future of hope
+            and dignity for all Palestinians. Your support enables us to
+            continue this vital work and stand in solidarity with the people of
+            Palestine.
           </p>
         </div>
         <div className="banner">
-          <img src="/about.png" alt="aboutimg" />
+          <img src="/flagm.png" alt="aboutimg" />
         </div>
       </div>
     </section>
